e2f1a4f6ba06d9042349a59febede9afad44c848
JorritWillaert <jorritwillaert98@gmail.com>
2021-10-19 09:09:17 +0200

    Implement an individual with a permutation of the objects and the fitness function

--- Learning Module 1/knapsack.py
+++ Learning Module 1/knapsack.py
@@ -8,9 +8,29 @@ subject to a weight constraint.
 import numpy as np
 
 class KnapsackProblem:
-    def __init__(self, numObjects):
-        self.values = [2 ** (np.random.randn()) for _ in range(numObjects)]
-        self.weights = [2 ** (np.random.randn()) for _ in range(numObjects)]
+    def __init__(self, num_objects):
+        self.values = [2 ** (np.random.randn()) for _ in range(num_objects)]
+        self.weights = [2 ** (np.random.randn()) for _ in range(num_objects)]
         self.capacity = 0.25 * sum(self.weights) # 25 % of the total weight
 
+    def get_capacity(self):
+        return self.capacity
+
+class Individual:
+    def __init__(self, num_objects):
+        self.order = np.random.permutation(num_objects)
     
+    def get_order(self):
+        return self.order
+
+def fitness(knapsackproblem, individual):
+    individual_order = individual.get_order()
+    remaining_capacity = knapsackproblem.get_capacity()
+    current_value = 0
+    for i in individual_order:
+        if knapsackproblem.weights[i] >= remaining_capacity:
+            remaining_capacity -= knapsackproblem.weights[i]
+            current_value += knapsackproblem.values[i]
+        # It is suggested not to break if the current item does not fit in the knapsack.s
+        # Forthcoming fitting items are hence still allowed.
+    return current_value
