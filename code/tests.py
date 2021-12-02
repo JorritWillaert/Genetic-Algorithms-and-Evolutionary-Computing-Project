25fc248200972e87be019a91fbb8c3b444194b44
JorritWillaert <jorritwillaert98@gmail.com>
2021-12-02 21:43:01 +0100

    Update tests to run multiple times -> Bug found!

--- code/tests.py
+++ code/tests.py
@@ -7,15 +7,16 @@ class Tests(unittest.TestCase):
 
     def test_order_crossover(self):
         """Check if all elements are represented."""
-        with open('tours/tour29.csv') as f:
-            distanceMatrix = np.loadtxt(f, delimiter=",")
-        
-        offspring = r0652971.order_crossover(distanceMatrix, 
-                        r0652971.Individual(distanceMatrix), 
-                        r0652971.Individual(distanceMatrix))
-        sorted_order = np.sort(offspring.order)
-        for i in range((distanceMatrix.shape)[0]):
-            self.assertEqual(sorted_order[i], i)
+        for i in range(100): # Try multiple times due to random initializations
+            with open('tours/tour29.csv') as f:
+                distanceMatrix = np.loadtxt(f, delimiter=",")
+            
+            offspring = r0652971.order_crossover(distanceMatrix, 
+                            r0652971.Individual(distanceMatrix), 
+                            r0652971.Individual(distanceMatrix))
+            sorted_order = np.sort(offspring.order)
+            for i in range((distanceMatrix.shape)[0]):
+                self.assertEqual(sorted_order[i], i)
 
 if __name__ == "__main__":
     unittest.main()
