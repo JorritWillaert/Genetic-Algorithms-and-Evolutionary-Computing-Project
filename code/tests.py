8976ac116415f38caa445796ea7e54ae34f13d50
JorritWillaert <jorritwillaert98@gmail.com>
2021-12-03 14:12:05 +0100

    Add test for edge crossover - still problem with -1

--- code/tests.py
+++ code/tests.py
@@ -18,5 +18,19 @@ class Tests(unittest.TestCase):
             for i in range((distanceMatrix.shape)[0]):
                 self.assertEqual(sorted_order[i], i)
 
+    def test_edge_crossover(self):
+        """Check if all elements are represented."""
+        for i in range(100):
+            with open('tours/tour29.csv') as f:
+                distanceMatrix = np.loadtxt(f, delimiter=",")
+            
+            offspring = r0652971.edge_crossover(distanceMatrix, 
+                            r0652971.Individual(distanceMatrix),
+                            r0652971.Individual(distanceMatrix))
+            sorted_order = np.sort(offspring.order)
+            for i in range((distanceMatrix.shape)[0]):
+                self.assertEqual(sorted_order[i], i)
+                
+
 if __name__ == "__main__":
     unittest.main()
